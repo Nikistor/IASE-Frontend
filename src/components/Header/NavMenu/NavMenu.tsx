13198259b43c8d2624aaa6613888d4369feb2e63
Nikistor <113507330+Nikistor@users.noreply.github.com>
2025-04-25 20:51:38 +0300

    Добавлена кнопка подключения к Streamline

--- src/components/Header/NavMenu/NavMenu.tsx
+++ src/components/Header/NavMenu/NavMenu.tsx
@@ -1,12 +1,12 @@
 import "./NavMenu.sass"
-import {Link} from "react-router-dom";
-import {useAuth} from "../../../hooks/users/useAuth";
-import {useEffect, useState} from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../../hooks/users/useAuth";
+import { useEffect, useState } from "react";
 import Hamburger from "../Hamburger/Hamburger";
 
 const NavMenu = () => {
 
-    const {is_authenticated, is_moderator, auth, user_name} = useAuth()
+    const { is_authenticated, is_moderator, auth, user_name } = useAuth()
 
     const [isOpen, setIsOpen] = useState(false)
 
@@ -18,28 +18,35 @@ const NavMenu = () => {
         <div>
             <div className={"menu-wrapper " + (isOpen ? "open" : "")}>
                 {is_moderator &&
-                <Link to="/companies_table" className="menu-item" onClick={(e) => {setIsOpen(false)}} >
-                    <span>Таблица городов</span>
-                </Link>
+                    <>
+                        {/* Новая кнопка для анализа данных компании */}
+                        <a href="http://localhost:8501" target="_blank" rel="noopener noreferrer" className="menu-item" onClick={(e) => { setIsOpen(false) }} >
+                            <span>Анализ данных</span>
+                        </a>
+
+                        <Link to="/cities_table" className="menu-item" onClick={(e) => { setIsOpen(false) }} >
+                            <span>Таблица компании</span>
+                        </Link>
+                    </>
                 }
-                <Link to="/companies" className="menu-item" onClick={(e) => {setIsOpen(false)}} >
+                <Link to="/companies" className="menu-item" onClick={(e) => { setIsOpen(false) }} >
                     <span>Города</span>
                 </Link>
 
                 {is_authenticated &&
-                    <Link to="/vacancies" className="menu-item" onClick={(e) => {setIsOpen(false)}} >
+                    <Link to="/vacancies" className="menu-item" onClick={(e) => { setIsOpen(false) }} >
                         <span>Вакансии</span>
                     </Link>
                 }
 
                 {is_authenticated &&
-                    <Link to="/profile" className="menu-item" onClick={(e) => {setIsOpen(false)}} >
+                    <Link to="/profile" className="menu-item" onClick={(e) => { setIsOpen(false) }} >
                         <span>{user_name}</span>
                     </Link>
                 }
 
                 {!is_authenticated &&
-                    <Link to="/login" className="menu-item" onClick={(e) => {setIsOpen(false)}} >
+                    <Link to="/login" className="menu-item" onClick={(e) => { setIsOpen(false) }} >
                         <span>Вход</span>
                     </Link>
                 }
